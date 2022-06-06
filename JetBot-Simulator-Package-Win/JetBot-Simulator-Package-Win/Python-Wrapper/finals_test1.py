154ef78e408fc2365d0898710c99284d90045c61
Renewrr <46344868+renewrr@users.noreply.github.com>
2022-06-06 22:37:13 +0800

    P_controller
    Proportional control for smoother movement.

--- JetBot-Simulator-Package-Win/JetBot-Simulator-Package-Win/Python-Wrapper/finals_test1.py
+++ JetBot-Simulator-Package-Win/JetBot-Simulator-Package-Win/Python-Wrapper/finals_test1.py
@@ -4,6 +4,8 @@ import cv2
 from time import perf_counter
 
 prev_time = perf_counter()
+WIDTH = 1280
+HALF_WIDTH = 640
 # KERNEL = np.ones((5,5),np.uint8)
 def execute(change):
     global prev_time
@@ -13,6 +15,7 @@ def execute(change):
     hsv_frame = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2HSV)
     red_frame = cv2.inRange(hsv_frame, (160, 50, 20), (179, 255, 255))
     red_frame = red_frame + cv2.inRange(hsv_frame, (0,50,20), (10,255,255))
+    black_frame = cv2.inRange(hsv_frame,(0,0,0),(180,255,150))
     # print(red_frame.shape)
     last_row = red_frame[-1,:]
 
@@ -27,13 +30,26 @@ def execute(change):
                 mid = (x+left)//2
         else:
             left = -1
-    if mid > 600:
-        robot.right(0.05)
-    elif mid < 560:
-        robot.left(0.05)
+    mid_diff = abs(mid-HALF_WIDTH)/HALF_WIDTH
+    robot.forward(0.2)
+    if mid > HALF_WIDTH+20:
+        # robot.right(0.05)
+        robot.add_motor(0.05*mid_diff,-0.05*mid_diff)
+    elif mid < HALF_WIDTH-20:
+        robot.add_motor(-0.05*mid_diff,0.05*mid_diff)
+        # robot.left(0.05)
+
+    red_frame_out = cv2.cvtColor(red_frame,cv2.COLOR_GRAY2BGR)
+    out_str = str(mid)
+    if mid > HALF_WIDTH+20:
+        out_str = "   " + out_str + ">>>"
+    elif mid < HALF_WIDTH-20:
+        out_str = "<<<" + out_str + "   "
     else:
-        robot.forward(0.2)
-    cv2.imshow("camera", red_frame)
+        out_str = "   " + out_str + "   "
+    cv2.rectangle(red_frame_out,(mid-10,700),(mid+10,720),(0,0,255),3)
+    cv2.putText(red_frame_out,out_str,(mid-75,680),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),1,cv2.LINE_AA)
+    cv2.imshow("camera", red_frame_out)
 
     prev_time = curr_time
 robot = Robot()
