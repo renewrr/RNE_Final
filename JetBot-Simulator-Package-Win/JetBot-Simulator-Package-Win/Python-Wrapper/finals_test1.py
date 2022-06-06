777f4c9fb6d2dd302c18eb00a21ec8210747ecbd
Renewrr <46344868+renewrr@users.noreply.github.com>
2022-06-06 23:39:10 +0800

    Wind down counter
    Added WindDown class for smoother stopping.

--- JetBot-Simulator-Package-Win/JetBot-Simulator-Package-Win/Python-Wrapper/finals_test1.py
+++ JetBot-Simulator-Package-Win/JetBot-Simulator-Package-Win/Python-Wrapper/finals_test1.py
@@ -9,7 +9,19 @@ HEIGHT = 720
 HALF_WIDTH = 640
 STOP_FLAG = 0
 LAST_ROW_BIAS = -250 # +250 to 250 should be safely within boundry
-# KERNEL = np.ones((5,5),np.uint8)
+class WindDown:
+    def __init__(self,speed) -> None:
+        self.total_steps = 30
+        self.steps = self.total_steps
+        self.original_speed = speed
+    def reset(self):
+        self.steps = self.total_steps
+    def next_value(self):
+        out = self.original_speed*(self.steps/self.total_steps)
+        if self.steps > 0:
+            self.steps -= 1
+        return out
+
 def execute(change):
     global prev_time,STOP_FLAG
     curr_time = perf_counter()
@@ -19,9 +31,8 @@ def execute(change):
     red_frame = cv2.inRange(hsv_frame, (160, 50, 20), (179, 255, 255))
     red_frame = red_frame + cv2.inRange(hsv_frame, (0,50,20), (10,255,255))
     black_frame = cv2.inRange(hsv_frame,(0,0,0),(180,255,150))
-    # print(red_frame.shape)
     last_row = red_frame[LAST_ROW_BIAS,:]
-    lateral_bias = -250
+    lateral_bias = 0
     left = -1
     best_len,mid = 0,-1
     for x,dot in enumerate(last_row):
@@ -36,12 +47,15 @@ def execute(change):
     target = HALF_WIDTH+lateral_bias
     mid_diff = abs(mid-target)/WIDTH
     if best_len == 0:
-        robot.stop()
+        # robot.stop()
+        robot.forward(wind.next_value())
         STOP_FLAG = 5
     elif STOP_FLAG > 0:
+        robot.forward(wind.next_value())
         STOP_FLAG -= 1
     else:
         robot.forward(0.2)
+        wind.reset()
         if mid > target+20:
             robot.add_motor(0.05*mid_diff,-0.05*mid_diff)
         elif mid < target-20:
@@ -63,4 +77,5 @@ def execute(change):
     prev_time = curr_time
 robot = Robot()
 camera = Camera()
+wind = WindDown(0.2)
 camera.observe(execute)
