49d81b667d762cbadc4d7a1b41a59a6ec85a9d04
HosinLau <46102770+HosinLau@users.noreply.github.com>
2022-06-15 22:36:14 +0800

    Update optical_flow_obstacle_detection.py

--- optical_flow_obstacle_detection.py
+++ optical_flow_obstacle_detection.py
@@ -2,11 +2,15 @@ import cv2
 import numpy as np
 import math
 
+directory = 'pass_left/'
+
 def OpticalFlow(i): # assign an image to track optical flow
     global argmax_x_start, argmax_x_end, argmax_y_start, argmax_y_end, OBSTACLE
     points = 2000 # [tunable parameter 2000]
-    before = cv2.imread('object' + str(i) + '.png')
-    after = cv2.imread('object' + str(i+1) + '.png')
+    before = cv2.imread(directory + 'object' + str(i) + '.png')
+    after = cv2.imread(directory + 'object' + str(i+1) + '.png')
+    original = cv2.imread(directory + 'object' + str(i+1) + '.png')
+    small = cv2.resize(original, (80,45))
     before = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
     after = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
     corners = cv2.goodFeaturesToTrack(before, points, 0.01, 10)
@@ -15,7 +19,8 @@ def OpticalFlow(i): # assign an image to track optical flow
     old = [(int(corners[p][0][0]), int(corners[p][0][1])) for p in range(points)]
     new = [(int(nextPts[p][0][0]), int(nextPts[p][0][1])) for p in range(points)]
     vector_x = [new[p][0] - old[p][0] for p in range(points)] # vector_x = new_x - old_x
-    obstacle_map = np.array([[0 for x in range(80)] for y in range(45)])
+    obstacle_map = np.array([[0.0 for x in range(80)] for y in range(45)])
+    drawn = [False for p in range(points)]
     #print(corners)
     #print(nextPts)
     #print(status)
@@ -31,16 +36,26 @@ def OpticalFlow(i): # assign an image to track optical flow
             continue
         cv2.line(before, old[p], new[p], (0, 255, 0), 2)
         cv2.line(after, old[p], new[p], (0, 255, 0), 2)
-        if abs(vector_x[p]) > 20: # notable object points [tunable parameter 20]
-            obstacle_map[min(44, int(new[p][1] / 16))][min(79, int(new[p][0] / 16))] += 1
-            obstacle_map[min(44, int(old[p][1] / 16))][min(79, int(old[p][0] / 16))] += 1
+        drawn[p] = True
+
+    mean = np.mean([abs(vector_x[p[0]]) for p in np.argwhere(drawn).tolist()])
+    stdev = np.std([abs(vector_x[p[0]]) for p in np.argwhere(drawn).tolist()])
+    concentration_map = np.array([[0.0 for x in range(80)] for y in range(45)])
+    for p in range(points):
+        if drawn[p] and abs(vector_x[p]) > 10: # notable object points [tunable parameter 10]
+            value = abs(vector_x[p]) / 20 # [tunable parameter 'value']
+            obstacle_map[min(44, int(new[p][1] / 16))][min(79, int(new[p][0] / 16))] += value
+            obstacle_map[min(44, int(old[p][1] / 16))][min(79, int(old[p][0] / 16))] += value
+            concentration_map[min(44, int(new[p][1] / 16))][min(79, int(new[p][0] / 16))] += np.power(2, (abs(vector_x[p]) - mean) / (stdev + 0.01)) # [tunable parameter 2]
+            concentration_map[min(44, int(old[p][1] / 16))][min(79, int(old[p][0] / 16))] += np.power(2, (abs(vector_x[p]) - mean) / (stdev + 0.01)) # [tunable parameter 2]
 
     heat_y = [0 for y in range(45)]
     accum_y = 0
     max_y = 0
     temp_start_y = 0
     for y in range(45):
-        heat_y[y] = 1 if np.argwhere(obstacle_map[y][:]).shape[0] >= 5 else -1  # [tunable parameter 5]
+        #print(y, np.argwhere(obstacle_map[y][:]).shape[0])
+        heat_y[y] = 1 if np.argwhere(obstacle_map[y][:]).shape[0] >= 12 else -1  # [tunable parameter 12]
         accum_y += heat_y[y]
         if accum_y < 0:
             accum_y = 0
@@ -55,7 +70,8 @@ def OpticalFlow(i): # assign an image to track optical flow
     max_x = 0
     temp_start_x = 0
     for x in range(80):
-        heat_x[x] = 1 if np.argwhere([obstacle_map[y][x] for y in range(45)]).shape[0] >= 2 else -1 # [tunable parameter 2]
+        #print(x, np.argwhere([obstacle_map[y][x] for y in range(45)]).shape[0])
+        heat_x[x] = 1 if np.argwhere([obstacle_map[y][x] for y in range(45)]).shape[0] >= 7 else -1 # [tunable parameter 7]
         accum_x += heat_x[x]
         if accum_x < 0:
             accum_x = 0
@@ -65,6 +81,34 @@ def OpticalFlow(i): # assign an image to track optical flow
             max_x = accum_x
             argmax_x_end = x
             argmax_x_start = temp_start_x
+
+    left_red = None
+    right_red = None
+    for x in range(argmax_x_start, argmax_x_end):
+        blue = int(small[argmax_y_end][x][0])
+        green = int(small[argmax_y_end][x][1])
+        red = int(small[argmax_y_end][x][2])
+        if red < 50: # [tunable parameter 50]
+            continue
+        if (red > green * 1.4 and red > blue * 1.4) or red > (green + blue) * 0.8: # [tunable parameters 1.4, 1.4, 0.8]
+            left_red = x
+            print(small[argmax_y_end][x])
+            break
+    for x in range(argmax_x_end, argmax_x_start, -1):
+        blue = int(small[argmax_y_end][x][0])
+        green = int(small[argmax_y_end][x][1])
+        red = int(small[argmax_y_end][x][2])
+        if red < 50: # [tunable parameter 50]
+            continue
+        if (red > green * 1.4 and red > blue * 1.4) or red > (green + blue) * 0.8: # [tunable parameters 1.4, 1.4, 0.8]
+            right_red = x
+            print(small[argmax_y_end][x])
+            break
+    if left_red is not None and right_red is not None:
+        if left_red - argmax_x_start > argmax_x_end - right_red:
+            argmax_x_end = left_red
+        else:
+            argmax_x_start = right_red
     cv2.rectangle(after, (argmax_x_start * 16, argmax_y_start * 16), (argmax_x_end * 16, argmax_y_end * 16), (0, 255, 0), 3)
     print(argmax_x_start, argmax_y_start, argmax_x_end, argmax_y_end)
 
@@ -73,22 +117,32 @@ def OpticalFlow(i): # assign an image to track optical flow
     if area < 100: # [tunable parameter 100]
         OBSTACLE = False
     else:
-        misses = np.argwhere([[obstacle_map[y][x] <= 0 for x in range(argmax_x_start, argmax_x_end)] for y in range(argmax_y_start, argmax_y_end)]).shape[0]  # [tunable parameter 1]
-        miss_rate = misses / area
-        print('Miss rate: ', miss_rate)
+        # miss rate and capture rate tells about the false positive and false negative of accurately cropping the flow, not the real obstacle
+        misses = np.argwhere([[obstacle_map[y][x] <= 1.2 for x in range(argmax_x_start, argmax_x_end)] for y in range(argmax_y_start, argmax_y_end)]).shape[0]  # [tunable parameter 1.2]
+        miss_rate = round(misses / area, 3)
+        print('False positive: ', miss_rate)
+        all_flow = np.sum([[concentration_map[y][x] for x in range(80)] for y in range(45)])
+        cropped_flow = np.sum([[concentration_map[y][x] for x in range(argmax_x_start, argmax_x_end)] for y in range(argmax_y_start, argmax_y_end)])
+        capture_rate = round(cropped_flow / all_flow, 3)
+        print('False negative: ', 1 - capture_rate)
+        cv2.rectangle(after, (0, 0), (650, 70), (0, 0, 0), -1)
+        rates_text = 'False positive: ' + str(miss_rate) + '   False negative: ' + str(round(1 - capture_rate, 3))
+        cv2.putText(after, rates_text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2, cv2.LINE_AA)
         if miss_rate > 0.45: # [tunable parameter 0.45]
             OBSTACLE = False
-        elif argmax_x_end - argmax_x_start < 10 or argmax_y_end - argmax_y_start < 5: # [tunable parameters 10, 5]
+        elif 1 - capture_rate > 0.4: # [tunable parameter 0.4]
+            OBSTACLE = False
+        elif argmax_x_end - argmax_x_start < 10 or argmax_y_end - argmax_y_start < 5 or argmax_x_end - argmax_x_start > 40: # [tunable parameters 10, 5, 40]
             OBSTACLE = False
         else:
             OBSTACLE = True
             print('Obstacle detected')
-        
             
     #cv2.imshow('before', before)
-    cv2.imshow('after', after)
-    cv2.imwrite('flow' + str(i) + '.png', after)
+    #cv2.imshow('after', after)
+    cv2.imwrite(directory + 'flow' + str(i) + '.png', after)
     return obstacle_map
 
 
-OpticalFlow(5)
+for i in range(20):
+    OpticalFlow(i+1)
